40669482f2f21ab6d1d615960b36a0aac08bcae7
marz982 <127357546+marz982@users.noreply.github.com>
2023-03-08 20:33:19 -0300

    initial commit

---
+++ Proyecto final/js/funciones.js
@@ -0,0 +1,10 @@
+
+document.getElementById( "boton").onclick =function () {
+    document.body.style.backgroundColor = "white"
+};
+
+document.addEventListener("click", function(){
+    console.log("hola mundo!")
+    
+})
+
